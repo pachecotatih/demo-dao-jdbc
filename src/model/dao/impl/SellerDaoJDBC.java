452bdbc00cdb70faba07b8076fc1cd9ab5bdf88e
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-06-15 11:35:51 -0300

    Add SellerDao and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import java.util.List;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller seller) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void update(Seller seller) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
