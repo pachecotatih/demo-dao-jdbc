8c6d6b775b043c05a50ef3cd2d9fef3cc3d02794
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-06-15 11:51:03 -0300

    findById Implementation Seller

--- src/application/App.java
+++ src/application/App.java
@@ -1,9 +1,14 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-       
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
     }
 }
